1860addb222a96a2f1adafdb3b92dcd5c506d634
jiang yadong <18827637106@163.com>
2023-08-27 16:12:31 +0800

    vue3

---
+++ src/views/jyd/pay/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="pay-wrap">
+        工资条
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+.pay-wrap {
+    padding:10px;
+}
+</style>
